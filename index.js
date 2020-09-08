e251f8833b3722b62cafa8a607eedc2b05ff9efa
Jose Trejo <joseatrejos@gmail.com>
2020-09-07 18:11:01 -0700

    Block programming

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import PKG from './package.json';
+import Block from './src/blockchain/block'
 
 const { name, version, description, main, repository, author, license  } = PKG;
+const { genesis } = Block;
 
+// console.log( `${description} ${author}` );
 
+const block = new Block(Date.now(), '8u8u1u8u', 'h4sh', 'd4t4');
+console.log( block.toString() );
 
-console.log( `${description} ${author}` );
+const block_2 = new Block(Date.now(), genesis.hash, 'h4sh2', 'transaccion1');
+console.log(block_2.toString());
