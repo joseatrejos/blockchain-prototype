95bd2509ae002fa798f1c45151326f9414d1e35d
Jose Trejo <joseatrejos@gmail.com>
2020-09-10 11:47:50 -0700

    Added real encription to the hashed

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { genesis } = Block;
 
 // console.log( `${description} ${author}` );
 
-const block = new Block(Date.now(), '8u8u1u8u', 'h4sh', 'd4t4');
-console.log( block.toString() );
+const block1 = Block.mine(genesis, 'transact1');
+console.log( block1.toString() );
 
-const block_2 = new Block(Date.now(), genesis.hash, 'h4sh2', 'transaccion1');
-console.log(block_2.toString());
+const block2 = Block.mine(block1, 'transact2');
+console.log( block2.toString() );
--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -1,3 +1,5 @@
+import { SHA256 } from 'crypto-js';
+
 class Block {
     
     constructor(timestamp, previousHash, hash, data) {
@@ -10,6 +12,18 @@ class Block {
     static get genesis() {
         const timestamp = (new Date(2000, 0, 1)).getTime();
         return new this(timestamp, undefined, 'g3n3s1s-h4sh', 'transaccion0')
+    }
+
+    static mine(previousBlock, data) {
+        const timestamp = Date.now();
+        const { hash: previousHash } = previousBlock;
+        const hash = Block.hash(timestamp, previousHash, data);
+        
+        return new this(timestamp, previousHash, hash, data);
+    }
+
+    static hash(timestamp, previousHash, data) {
+        return SHA256(`${timestamp}${previousHash},${data}`).toString();
     }
 
     toString() {
